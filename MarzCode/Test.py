c2bfa91a36c8923c9da036faca3b570b96dd9a90
mmcinnes123 <m.mcinnes1.22@abdn.ac.uk>
2025-04-21 11:36:16 +0100

    Added some tests to see what AeroPy funcs are available.

--- MarzCode/Test.py
+++ MarzCode/Test.py
@@ -1,4 +1,35 @@
+
+from pythonnet import load
+
+from Export.CsvWriter import CsvWriter
+
+load("coreclr")
+import clr
+import csv
+
+clr.AddReference("resources\DelsysAPI")
+clr.AddReference("System.Collections")
+
+
+from Aero import AeroPy
+
+# Create an instance
+trigno = AeroPy()
+
+# # List all methods
+# methods = [method for method in dir(trigno) if not method.startswith('__')]
+# for method in methods:
+#     print(method)
+
+# # Get help on the object
+# help(trigno.AvailibleSensorModes)
+
+# Let's try to get the modes for a sensor (let's say sensor index 0)
 try:
-    from Aero import AeroPy
-except ImportError:
-    from . import AeroPy
+    modes = trigno.AvailibleSensorModes(0)
+    print("Available modes:", modes)
+except Exception as e:
+    print("Error:", e)
+
+# You can also check the type of the return value
+print("Return type:", type(modes))
