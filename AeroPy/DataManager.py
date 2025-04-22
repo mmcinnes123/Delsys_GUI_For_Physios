92f8df82db33ddb487042be733b229520c252aba
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2025-04-22 16:57:29 +0100

    New commit, same as before.

--- AeroPy/DataManager.py
+++ AeroPy/DataManager.py
@@ -20,6 +20,9 @@ class DataKernel():
         """Processes the data from the DelsysAPI and place it in the data_queue argument"""
         outArr = self.GetData()
         if outArr is not None:
+
+            print(outArr[4][0]) # This is q_w   # TODO: remove this line
+
             for i in range(len(outArr)):
                 self.allcollectiondata[i].extend(outArr[i][0].tolist())
             try:
